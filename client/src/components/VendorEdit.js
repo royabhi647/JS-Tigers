b82986639db36fb0dbce46b2d0693d175c5547f0
royabhi647 <royabhi647@gmail.com>
2023-10-03 17:56:48 +0530

    vendor's management

--- client/src/components/VendorEdit.js
+++ client/src/components/VendorEdit.js
@@ -9,16 +9,16 @@ function VendorEdit() {
   const navigate = useNavigate();
   const [data, setData] = useState();
 
-  useEffect(() => {
-    getDetails();
-  }, [getDetails]);
-
   const getDetails = async () => {
     let result = await fetch(`http://localhost:5000/details/${params.id}`);
     result = await result.json();
     // console.log("result", result);
     setData(result);
   };
+
+  useEffect(() => {
+    getDetails();
+  }, [getDetails]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
