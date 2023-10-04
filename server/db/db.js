2049dded419737db985770b3df0dc1f6e89785f3
royabhi647 <royabhi647@gmail.com>
2023-10-04 10:43:03 +0530

    vendor management system

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/vendor-details", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/vendor-details")
   .then(() => console.log("Database is connected"))
   .catch((err) => console.log(err));
-
-// mongoose
-//   .connect("mongodb://localhost:27017/vendor-details")
-//   .then(() => console.log("Database is connected"))
-//   .catch((err) => console.log(err));
